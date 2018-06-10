769bbc7cb629b82efd3c5edbd5b6c06ae2ea9c61
iRuxu <rx6@qq.com>
2018-06-10 15:44:32 +0800

    task update

--- task/publish.js
+++ task/publish.js
@@ -1,4 +1,6 @@
 const shell = require('shelljs');
+const version = require('../package.json').version
 
-//shell.exec('npm publish')
-shell.exec('git status')
+shell.exec(`git tag ${version}`)
+shell.exec(`git push origin ${version}`)
+shell.exec('npm publish')

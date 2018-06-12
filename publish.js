6e063bd25b6dc10191a7fcbedf90dcc7f698817c
iRuxu <rx6@qq.com>
2018-06-13 00:15:15 +0800

    0.0.8

--- publish.js
+++ publish.js
@@ -1,5 +1,5 @@
 const shell = require('shelljs');
-const version = require('../package.json').version
+const version = require('package.json').version
 
 shell.exec(`git tag ${version}`)
 shell.exec(`git push origin ${version}`)
